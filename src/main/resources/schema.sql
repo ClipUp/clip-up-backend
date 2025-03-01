ba9c7ff10a191f3f94f35b9b03686e005ffcfefc
gitaepark97 <parkkitae7@gmail.com>
2025-03-01 10:15:05 +0900

    fix: schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,18 +2,18 @@ CREATE SCHEMA clip_up;
 
 CREATE TABLE clip_up.service_user
 (
-    id          VARCHAR(255) PRIMARY KEY,
-    email       VARCHAR(255) UNIQUE NOT NULL,
-    username    VARCHAR(100)        NOT NULL,
+    id          VARCHAR(20) PRIMARY KEY,
+    email       VARCHAR(100) UNIQUE NOT NULL,
+    username    VARCHAR(20)         NOT NULL,
     create_time BIGINT              NOT NULL,
     update_time BIGINT              NOT NULL
 );
 
 CREATE TABLE clip_up.login_info
 (
-    id        VARCHAR(255) PRIMARY KEY,
-    user_id   VARCHAR(255) NOT NULL,
-    method    VARCHAR(50)  NOT NULL,
+    id        VARCHAR(20) PRIMARY KEY,
+    user_id   VARCHAR(20)  NOT NULL,
+    method    VARCHAR(10)  NOT NULL,
     login_key VARCHAR(255) NOT NULL,
     password  VARCHAR(255),
     CONSTRAINT unique_method_login_key UNIQUE (method, login_key)
@@ -21,14 +21,14 @@ CREATE TABLE clip_up.login_info
 
 CREATE TABLE clip_up.meeting
 (
-    id                  VARCHAR(255) PRIMARY KEY,
-    owner_id            VARCHAR(255) NOT NULL,
-    title               VARCHAR(255) NOT NULL,
+    id                  VARCHAR(20) PRIMARY KEY,
+    owner_id            VARCHAR(20) NOT NULL,
+    title               VARCHAR(40) NOT NULL,
     audio_file_url      TEXT,
     audio_file_duration INTEGER,
     script              TEXT,
     minutes             TEXT,
-    create_time         BIGINT       NOT NULL,
-    update_time         BIGINT       NOT NULL,
-    is_deleted          BOOLEAN DEFAULT FALSE
+    create_time         BIGINT      NOT NULL,
+    update_time         BIGINT      NOT NULL,
+    is_deleted          BOOLEAN
 );
